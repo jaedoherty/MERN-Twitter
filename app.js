fe8505cdc176e1e155a630d0d415c461a69ce3ca
Jae Doherty <jaeld97@gmail.com>
2020-12-26 19:49:43 -0800

    fixed user schema

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// import UserSchema from './models/User'
+const UserSchema = require('./models/User')
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
@@ -12,10 +14,30 @@ mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
-    require('./models/User');
-    require('./models/Tweet');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+// const mongoose = require('mongoose');
+const User = mongoose.model('users', UserSchema);
+const keys = require('./config/keys');
 
+const options = {};
+options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+options.secretOrKey = keys.secretOrKey;
 
+module.exports = passport => {
+    passport.use(new JwtStrategy(options, (jwt_payload, done) => {
+        User.findById(jwt_payload.id)
+            .then(user => {
+                if (user) {
+                    // return the user to the frontend
+                    return done(null, user);
+                }
+                // return false since there is no user
+                return done(null, false);
+            })
+            .catch(err => console.log(err));
+    }));
+};
 app.get("/", (req, res) => res.send("Hello World!!"));
 
 app.use(passport.initialize());
